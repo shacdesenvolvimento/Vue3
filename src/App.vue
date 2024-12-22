b22bbca6342b39b3194b5df9a072082e4150bce5
matheusmg <matheus.gorchinsky@admnacional.com.br>
2024-12-22 19:27:59 -0300

    aula 01 a 05

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
+  <TheHeader v-if="Showreader" />
   <img alt="Vue logo" src="./assets/logo.png">
+<!--v-show similar if porem não tem else
+v-for é o loop do Vue
+-->
+  <div v-show="ShowName">
+    {{ FirstName }} <br>
+    {{ LastName }}
+  </div>
+  <div v-if="acesso=='admin'">Admin</div>
+  <div v-else-if="acesso=='marketing'">Marketing</div>
+  <div v-else>User</div>
+<!--
+v-for é o loop do Vue
+-->
+  <div 
+  v-for="(dados, index) in todos"
+  v-bind:key="dados.id"
+  class="para"
+  >
+  {{index}}  {{ dados.firstname }}
+    
+  </div>
+
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+//aqui estao os componentes importados que serão utilizados neste componetes
 import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from './components/TheHeader.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    //aqui estão as declarações dos componentes importados para que sejão reconhecidos
+    HelloWorld,
+    TheHeader
+  },
+  //data são todas as variaveis e objetos que serão utilizado no componente. Literlamente os dados
+  data() {
+    return {
+      Showreader: true,
+      FirstName: 'John',
+      LastName: 'Doe',
+      ShowName:false,
+      acesso: 'marketing',
+      todos:[
+      {"id":1,"firstname":"John","lastname":"Doe","email":"johndoe@example.com"},
+      {"id":2,"firstname":"Jane","lastname":"Smith","email":"janesmith@example.com"},
+      {"id":3,"firstname":"Bob","lastname":"Johnson","email":"bobjohnson@example.com"},
+      {"id":4,"firstname":"Emily","lastname":"Davis","email":"emilydavis@example.com"},
+      ]
+
+    }
+  },
 }
 </script>
 
@@ -22,5 +65,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.para {
+  margin: 60px;
+  background-color: blue;
+  color: antiquewhite;
+
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="tre">
+        Aqui estou
+    </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+    .tre {
+        background-color: black; 
+        color: white;
+         font-size: 24px;
+         text-align: center;
+ 
+    }
+</style>
